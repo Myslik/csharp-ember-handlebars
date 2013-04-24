c53b04ccdfdc903b5077c8bd2addfec8cc4d196d
Thiago Santos <thiago_trds@outlook.com>
2013-04-24 16:26:48 -0400

    Refactor Sample App
    Sample App now supports template pre-compilation only when not in debug
    mode (check is done via `HttpContext.Current.IsDebuggingEnabled`; same
    as Ember SPA template for Visual Studio).
    
    Comments were added to BundleConfig and README.md

---
+++ Ember.Handlebars.ExampleWebAPI/Helpers/EmberHelpers.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+public static class HtmlHelperExtensions
+{
+    private static string templateFolder = HttpContext.Current.Server.MapPath("scripts/app/templates");
+
+    public static MvcHtmlString RenderEmberTemplates(this HtmlHelper helper, string path = "", bool noTemplateName = false)
+    {
+        if (HttpRuntime.Cache[path] == null)
+        {
+            string absolutePath;
+            string templateName = "";
+            if (string.IsNullOrEmpty(path))
+            {
+                absolutePath = templateFolder;
+            }
+            else
+            {
+                templateName = path.Replace("\\", "-");
+                absolutePath = Path.Combine(templateFolder, path);
+            }
+
+            if (File.Exists(absolutePath))
+            {
+                int fileExtensionPosition = templateName.LastIndexOf('.');
+                if (fileExtensionPosition > 0)
+                {
+                    templateName = templateName.Substring(0, fileExtensionPosition);
+                }
+
+                string templateContent = ReadTemplate(templateName, new FileInfo(absolutePath), noTemplateName);
+
+                HttpRuntime.Cache.Insert(path, new MvcHtmlString(templateContent), new CacheDependency(absolutePath));
+            }
+            else
+            {
+                if (Directory.Exists(absolutePath))
+                {
+                    if (templateName.Length > 0 && templateName[templateName.Length - 1] != '-')
+                    {
+                        templateName += "-";
+                    }
+                    List<string> dependencyList = new List<string>();
+
+                    MvcHtmlString result = new MvcHtmlString(GetDirectoryTemplates(templateName, "", new DirectoryInfo(absolutePath), dependencyList));
+                    HttpRuntime.Cache.Insert(path, result, new CacheDependency(dependencyList.ToArray()));
+                }
+                else
+                {
+                    return new MvcHtmlString(""); //nothing is found, return empty string and do not cache
+                }
+            }
+        }
+
+        return HttpRuntime.Cache[path] as MvcHtmlString;
+    }
+
+    private static string GetDirectoryTemplates(string templateName, string relativeDirName, DirectoryInfo rootDirectory, List<string> dependencyList)
+    {
+        dependencyList.Add(rootDirectory.FullName);
+
+        var newSubRelativeDirName = relativeDirName;
+        if (!string.IsNullOrEmpty(newSubRelativeDirName))
+        {
+            newSubRelativeDirName = newSubRelativeDirName + "-";
+        }
+        var content = "";
+
+        foreach (DirectoryInfo subDir in rootDirectory.GetDirectories())
+        {
+            content += GetDirectoryTemplates(templateName, newSubRelativeDirName + subDir.Name, subDir, dependencyList);
+        }
+
+        foreach (FileInfo templateFile in rootDirectory.GetFiles())
+        {
+            string subtemplateName = templateFile.Name;
+            int fileExtensionPosition = subtemplateName.LastIndexOf('.');
+            if (fileExtensionPosition > 0)
+            {
+                subtemplateName = subtemplateName.Substring(0, fileExtensionPosition);
+            }
+            if (relativeDirName.Length > 0 && relativeDirName[relativeDirName.Length - 1] != '-')
+            {
+                relativeDirName += "-";
+            }
+            content += ReadTemplate(templateName + relativeDirName + subtemplateName, templateFile);
+        }
+        return content;
+    }
+
+    private static string ReadTemplate(string templateName, FileInfo templateFile, bool noTemplateName = false)
+    {
+        string content = File.ReadAllText(templateFile.FullName);
+        if (templateFile.Extension.ToLowerInvariant().StartsWith(".htm"))
+        {
+            //for .html/.htm* files, we'll simply return the content
+            return content;
+        }
+
+        //for other template files, we treate them as x-handlebars script type
+        if (noTemplateName)
+        {
+            return "<script type=\"text/x-handlebars\">\n" + content + "\n</script>\n";
+        }
+
+        templateName = templateName.Replace("-", "/");
+        var segments = templateName.Split('/');
+        if (segments.Length > 2) {
+            templateName = string.Format("{0}/{1}", segments[segments.Length - 2], segments[segments.Length - 1]);
+        }
+
+        return "<script type=\"text/x-handlebars\" data-template-name=\"" + templateName + "\">\n" + content + "\n</script>\n";
+    }
+
+}
--- README.md
+++ README.md
@@ -37,22 +37,90 @@ This library now has a built-in implementation of `IBundleTransform` which can b
 `BundleConfig` class as shown below:
 
 ```csharp
-bundles.Add(new Bundle("~/bundles/templates", new EmberHandlebarsBundleTransform())
-       .Include("~/scripts/app/templates/*.handlebars"));
+     bundles.Add(new Bundle("~/bundles/templates",
+             new EmberHandlebarsBundleTransform())
+            .IncludeDirectory("~/scripts/app/templates", "*.hbs", true)
+     );
 ```
 
-The `EmberHandlebarsBundleTransform` class has the flag `MinifyTemplates` which specifies whether or not the returning 
-JavaScript will be minified. The default is `true`, but if for some reason you need the template functions not to be 
-minified, do the following:
+In developmet, the template will be rendered directly in the cshtml view. See `~/Views/Home/Index.cshtml`
 
-```csharp
-bundles.Add(new Bundle("~/bundles/templates", 
-            new EmberHandlebarsBundleTransform() { MinifyTemplates = false })
-       .Include("~/scripts/app/templates/*.handlebars"));
-```
+    @if (HttpContext.Current.IsDebuggingEnabled) {
+        @Html.RenderEmberTemplates()
+    } else {
+        @Scripts.Render("~/bundles/templates")
+    }
+
+    @Scripts.Render("~/bundles/app")
+              
+To enable the bundle with pre-compilation of Handlebars templates, edit 
+the web.config to disable debug, setting it to false:
+         
+     <configuration>
+         ...
+         <system.web>
+             <compilation debug="false" targetFramework="4.5" />
+             ...
+                              
+Optimizations must be enabled in Global.asax.cs Application_Start method:
+             
+     BundleTables.EnableOptimizations = true;
+              
+The pre-compiled templates are minified by default, but if for some reason 
+one needs the template not to be minified, do:
+      
+     bundles.Add(new Bundle("~/bundles/templates",
+        new EmberHandlebarsBundleTransform() { minifyTemplates = false })
+        .IncludeDirectory("~/scripts/app/templates", "*.hbs", true)
+     ); 
+                 
+Template names must follow Ember.js Naming Conventions found in:
+ - http://emberjs.com/guides/concepts/naming-conventions/
+ - http://emberjs.com/guides/routing/defining-your-routes/
+                 
+Templates in sub directories will have their name appended to the parent directory, even if the 
+parent directory is also a sub-directory. Consider the following router map:
+                 
+     App.Router.map ->
+        @resource 'shows', ->
+            @route 'add'
+            @resource 'show', {path: ':show_id'}, ->
+                @route 'edit'
+                @route 'remove' 
+                  
+The directory structure for these routes could be:
+              
+     ~/
+      |_scripts
+        |_app
+          |_templates
+          |_shows
+            |_show
+            | |_default.hbs
+            | |_edit.hbs
+            | |_remove.hbs
+            |
+            |_index.hbs
+            |_add.hbs 
+            
+The templates will be compiled as:
+
+    ~/scripts/app/templates/shows/show/default.hbs  => Em.TEMPLATES["show"]
+    ~/scripts/app/templates/shows/show/edit.hbs     => Em.TEMPLATES["show/edit"]
+    ~/scripts/app/templates/shows/show/remove.hbs   => Em.TEMPLATES["show/remove"]
+    ~/scripts/app/templates/shows/index.hbs         => Em.TEMPLATES["shows/index"]
+    ~/scripts/app/templates/shows/add.hbs           => Em.TEMPLATES["shows/add"]
+                 
+Note that the templates named `default.hbs` under a directory `show` will be compiled as 
+`Em.TEMPLATES["show"]`. The same happens if the template has the same name of the parent
+directory: If that template has been named `show.hbs`, it would also be compiled as `Em.TEMPLATES["show"]`.
+                 
+If one does not need to separate templates in sub directories, but needs to name templates according to
+conventions, a character replacement will compile `show-edit` to `Em.TEMPLATES["show/edit"]` replacing
+the dash (`-`) with a slash (`/`) since Windows does not allow special characters in file or folder names.
 
 Note: The built-in `EmberHandlebarsBundleTransform` allows your templates to have whichever extension 
-that better suits your development process. The examples above use *.handlebars as an extension but it could be 
+that better suits your development process. The examples above use *.hbs as an extension but it could be 
 something else, like *.html for example.
 
 ## Creating a custom implementation of IBundleTransform
