77501b7e19d02b99637a9b0fb35eff129cdabd2a
Thiago Santos <thiago_trds@outlook.com>
2013-04-27 14:02:12 -0400

    Update Sample to Ember RC3
    - Updated sample to use Ember-RC3 via NuGet
    - .NET Bundle/Minifier breaks Ember and Ember-Data: Removed bunddles and
    added rederences to the files through script tags
    - Reorganized bundles and templates (partials)
    - Added ShowIndexRoute in show_route.js

--- Ember.Handlebars.ExampleWebAPI/Scripts/app/routes/show_route.js
+++ Ember.Handlebars.ExampleWebAPI/Scripts/app/routes/show_route.js
@@ -1,16 +1,23 @@
-﻿App.ShowRoute = Em.Route.extend({
+﻿App.ShowRoute = Em.Route.extend( {
+    model: function ( params ) {
+        return App.Show.find(params.show_id);
+    }
+} );
+
+
+App.ShowIndexRoute = Em.Route.extend( {
     activate: function () {
         this.controllerFor("application").set("notification", "");
         $(document).attr('title', 'Show Details');
     },
     model: function (params) {
-        return App.Show.find(params.show_id);
+        return this.modelFor('show');
     },
     setupController: function (controller, model) {
-        controller.set('content', model);
+        controller.set( 'content', model );
+        console.log( 'a' );
     },
     renderTemplate: function () {
-        this.render('show', { into: 'application' });
+        this.render('show/index', { into: 'application' });
     }
-
 });
